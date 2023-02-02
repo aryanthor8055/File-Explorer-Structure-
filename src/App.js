1d283760207769c2defb93da176b13e8e5f400b9
Aryan <aryan16072001236809@gmail.com>
2023-02-02 11:35:23 +0530

    Adding AddFolder and AddFile Functionality using CustomHook(useTraverseTree)---Finish

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import { useState } from 'react';
-import './App.css';
-import explorer from './data/folderData';
-import Folder from './components/Folder';
+import { useState } from "react";
+import Folder from "./components/Folder";
+import useTraverseTree from "./hooks/use-traverse-tree";
+import "./App.css";
+import explorer from "./data/folderData"
 
-function App() {
+export default function App() {
   const [explorerData, setExplorerData] = useState(explorer);
 
+  const { insertNode } = useTraverseTree();
+
+  const handleInsertNode = (folderId, item, isFolder) => {
+    const finalTree = insertNode(explorerData, folderId, item, isFolder);
+    setExplorerData(finalTree);
+  };
 
   return (
     <div className="App">
-      <Folder explorer={explorerData} />
+      <Folder handleInsertNode={handleInsertNode} explorer={explorerData} />
     </div>
   );
 }
-
-export default App;
